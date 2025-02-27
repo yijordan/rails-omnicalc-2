39b6c1bd8a4ff9b395f042f6b47a29e56534174a
yijordan <yij@uchicago.edu>
2025-02-27 06:15:31 +0000

    grrr cannot figure out this firstnum error for the life of me

--- app/controllers/subtraction_controller.rb
+++ app/controllers/subtraction_controller.rb
@@ -4,7 +4,7 @@ class SubtractionController < ApplicationController
   end
 
   def subtract_these
-    @first_number = params.fetch("first_num").to_f
+    @first_number = params.fetch("first").to_f
     @second_number = params.fetch("second_num").to_f
 
     @result =  @second_number - @first_number
